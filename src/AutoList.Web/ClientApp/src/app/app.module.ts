aad1f96dc55997dccf16eca8ba79c096e6a4a577
Adam Renaud <adamf.renaud@gmail.com>
2018-12-14 14:36:16 -0500

    Setting up the application

--- src/AutoList.Web/ClientApp/src/app/app.module.ts
+++ src/AutoList.Web/ClientApp/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
