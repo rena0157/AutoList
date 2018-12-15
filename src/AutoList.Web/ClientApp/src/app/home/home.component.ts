2b35ca6cfec5e4349e1ecddaf7711726bf598751
Adam Renaud <adamf.renaud@gmail.com>
2018-12-14 19:30:02 -0500

    Adding keyUp function to home component

--- src/AutoList.Web/ClientApp/src/app/home/home.component.ts
+++ src/AutoList.Web/ClientApp/src/app/home/home.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-    constructor() { }
+    constructor() {
+
+    }
+
+    /**
+     * Send the text from the input area to the
+     * AutoList Api and place the total length
+     * and total area into the corresponding
+     * text areas
+     * @param text The text from the textarea
+     */
+    OnInputAreaKeyUp(text:string) {
+        console.log(text);
+    }
 
     ngOnInit() {
+
     }
 
 }
